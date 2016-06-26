b4d0fd8c63398bb68682c9bfa5a50dc09e396bc4
Michael Sledge <msevilgenius@users.noreply.github.com>
2016-06-26 14:23:57 +0100

    begin structuring android code

--- android/ascores/app/src/main/java/net/sledgend/ascores/ScoreListActivity.java
+++ android/ascores/app/src/main/java/net/sledgend/ascores/ScoreListActivity.java
@@ -4,15 +4,21 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+import android.support.v7.widget.RecyclerView;
 
 import com.getpebble.android.kit.PebbleKit;
 
 public class ScoreListActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,14 @@ public class ScoreListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_score_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mRecyclerView = (RecyclerView) findViewById(R.id.score_list_view);
+
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new ScoreListAdapter();
+        mRecyclerView.setAdapter(mAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
---
+++ android/ascores/app/src/main/java/net/sledgend/ascores/ScoreListAdapter.java
@@ -0,0 +1,24 @@
+package net.sledgend.ascores;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by Michael on 22/06/2016.
+ */
+public class ScoreListAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
